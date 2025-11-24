da02d56ff4a18dfde48fbc1bece66bd13c7bb73a
gOnEbOnE <proaielaeu@gmail.com>
2025-11-24 11:28:27 +0700

    add cors

--- src/main/java/apap/ti/_5/vehicle_rental_2306245592_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/vehicle_rental_2306245592_be/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package apap.ti._5.vehicle_rental_2306245592_be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,19 +8,39 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-					.addMapping("/**")
-					.allowedOrigins("http://localhost:5173")
-					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-					.allowedHeaders("*")
-					.allowCredentials(true)
-					.exposedHeaders("Authorization");
-			}
-		};
-	}
+// 	@Bean
+// 	public WebMvcConfigurer corsConfigurer() {
+// 		return new WebMvcConfigurer() {
+// 			@Override
+// 			public void addCorsMappings(CorsRegistry registry) {
+// 				registry
+// 					.addMapping("/**")
+// 					.allowedOrigins("http://localhost:5173")
+// 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+// 					.allowedHeaders("*")
+// 					.allowCredentials(true)
+// 					.exposedHeaders("Authorization");
+// 			}
+// 		};
+// 	}
+// }
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String[] origins = allowedOrigins.split(",");
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
 }
